88fe1ac690fca56ecd305bf586e29097634e37e4
pietrostraneo <straneo_p@yahoo.it>
2023-12-18 16:25:40 +0100

    Fixed positions of dropdowns

--- js/script.js
+++ js/script.js
@@ -237,7 +237,12 @@ const { createApp } = Vue
             this.cursor.index = index;
         },
         deleteMsg(index){
+            console.log(this.contacts[this.currentUser].messages)
+            alert(`test + ${index}`)
             this.contacts[this.currentUser].messages.splice(index, 1);
+            console.log(this.contacts[this.currentUser].messages)
+            this.cursor.visible = false;
+            this.cursor.index = false;
         },
         deleteChat(){
             this.contacts.splice(this.currentUser, 1);
